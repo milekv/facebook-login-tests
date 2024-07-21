c2162a3753c6729d1f0444f069e16d9858da655c
Miłosz Kordziński <miloszk.kontakt@gmail.com>
2024-07-21 20:25:42 +0200

    Dodanie testów logowania do Facebooka

--- test_login.py
+++ test_login.py
@@ -1,40 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 def test_valid_login():
-  driver = webdriver.Chrome()  
-  driver.get("https://www.facebook.com/")
+    driver = webdriver.Chrome()
+    driver.get("https://www.facebook.com/")
 
-  email_field = driver.find_element(By.ID, "email")
-  password_field = driver.find_element(By.ID, "pass")
-  login_button = driver.find_element(By.NAME, "login")
+    
+    try:
+        wait = WebDriverWait(driver, 5)
+        cookie_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Allow all cookies')]")))
+        cookie_button.click()
+    except:
+        pass
 
-  email_field.send_keys("facebook_email")  
-  password_field.send_keys("facebook_password")  
-  login_button.click()
+    email_field = driver.find_element(By.ID, "email")
+    password_field = driver.find_element(By.ID, "pass")
+    login_button = driver.find_element(By.NAME, "login")
 
-  time.sleep(5)  
+    email_field.send_keys("facebook_email")  
+    password_field.send_keys("facebook_password")  
+    login_button.click()
 
-    assert "facebook.com/home.php" in driver.current_url  
+    time.sleep(5)  
 
-  driver.quit()
+    assert "facebook.com/home.php" in driver.current_url
 
-def test_invalid_login():
-  driver = webdriver.Chrome()
-  driver.get("https://www.facebook.com/")
-
-  email_field = driver.find_element(By.ID, "email")
-  password_field = driver.find_element(By.ID, "pass")
-  login_button = driver.find_element(By.NAME, "login")
-
-  email_field.send_keys("invalid_email@example.com")  
-  password_field.send_keys("invalid_password")  
-  login_button.click()
-
-  time.sleep(5)
-
-  error_message = driver.find_element(By.XPATH, "//div[contains(text(), 'The password that you\'ve entered is incorrect.')]")  
-  assert error_message.is_displayed()
-
-  driver.quit()
+    driver.quit()
